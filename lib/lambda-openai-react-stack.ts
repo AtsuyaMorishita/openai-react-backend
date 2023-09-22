02f9e4f922e84ea9d14829dab850bbe42b54bbb8
AtsuyaMorishita <mori.a0426@gmail.com>
2023-09-22 09:06:51 +0900

    apiを叩く処理

--- lib/lambda-openai-react-stack.ts
+++ lib/lambda-openai-react-stack.ts
@@ -1,14 +1,14 @@
 import * as cdk from "aws-cdk-lib";
-import { AssetCode, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
-import { join } from "path";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class LambdaOpenaiReactStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    //lambda関数の設定
     new NodejsFunction(this, "lambda", {
       entry: "src/handler.ts", // lambda 関数のエントリーポイント
       handler: "handler", // 実行する関数名
--- src/handler.ts
+++ src/handler.ts
@@ -1,7 +1,37 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+//openAIの初期設定
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 export const handler = async function (event: any, context: any) {
-  return {
-    statusCode: 200,
-    headers: {},
-    body: { message: "こちらでAPIを作成します！" },
-  };
+  const pushMessage = event.message;
+
+  try {
+    const completion: any = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          //TODO:返答の文字数を制限する
+          role: "user", // "user" | "assistant" | "system"
+          content: pushMessage, // string
+        },
+      ],
+    });
+    const getMessage = completion.data.choices[0].message.content;
+
+    return {
+      statusCode: 200,
+      headers: {},
+      body: { message: getMessage },
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: { message: "エラーが発生しました" },
+    };
+  }
 };
